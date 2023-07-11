d6914cb5e4ddd18dbd9fa2ef88621bfaec7e2783
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-11 00:41:43 -0700

    Direct
    Direct exanchage was added

---
+++ Exchanges/Direct/Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Producer");
+
+var factory = new ConnectionFactory();
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("orders_direct",type:ExchangeType.Direct, durable:true);
+
+channel.BasicPublish(exchange:"orders_direct",routingKey:"confirmed",null,Encoding.UTF8.GetBytes("order1 confirmed"));
+
+channel.BasicPublish(exchange:"orders_direct",routingKey:"created",null,Encoding.UTF8.GetBytes("order2 created"));
+
+
+Console.ReadKey();
