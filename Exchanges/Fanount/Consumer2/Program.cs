448f65f6511943725b160f657a71d8929971bfbd
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-11 00:05:54 -0700

    Fanout
    Fanout exchange was added in order to test

---
+++ Exchanges/Fanount/Consumer2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Consumer2");
+
+var factory = new ConnectionFactory();
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("orders",type:ExchangeType.Fanout,durable:true);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queueName,"orders",string.Empty,null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine(message + " received");
+};
+
+channel.BasicConsume(consumer,queueName,autoAck:true);
+
+Console.ReadLine();
---
+++ Exchanges/Fanount/Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Producer");
+
+var factory = new ConnectionFactory();
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("orders", type:ExchangeType.Fanout, durable:true);
+
+var body = Encoding.UTF8.GetBytes("order1");
+
+channel.BasicPublish(exchange:"orders", routingKey:string.Empty, null,body);
+
+Console.ReadKey();
