03d726fa724c2f989f16a1637b55871a161d72ca
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-10 23:33:38 -0700

    ConsoleApps were added

---
+++ ConsoleApps/App2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+var connection = factory.CreateConnection();
+
+var channel1 = connection.CreateModel();
+
+channel1.QueueDeclare("order_queue",false,false,false,null);
+
+
+var consumer = new EventingBasicConsumer(channel1);
+
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine(message + "received");
+
+    channel1.BasicAck(args.DeliveryTag,false);
+};
+
+channel1.BasicConsume(consumer, "order_queue",false);
+
+Console.ReadLine();
---
+++ ConsoleApps/App3/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest",
+    DispatchConsumersAsync = true
+};
+
+var connection = factory.CreateConnection();
+
+var channel1 = connection.CreateModel();
+
+channel1.QueueDeclare("message",false,false,false,null);
+var consumer = new AsyncEventingBasicConsumer(channel1);
+
+consumer.Received += async (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine(message + "saving...");
+
+    await SendMessage();
+};
+
+channel1.BasicConsume(consumer,"message",false);
+
+Console.ReadKey();
+
+async Task SendMessage()
+{
+    await Task.Delay(1000);
+    Console.WriteLine("saved");
+}
