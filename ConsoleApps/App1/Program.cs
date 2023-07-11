8218f769708c5a5a75a54961b3ac0053ef9c240b
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-10 23:40:31 -0700

    ok

---
+++ ConsoleApps/App1/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+var connection = factory.CreateConnection();
+
+var channel1 = connection.CreateModel();
+
+channel1.QueueDeclare("order_queue", false, false, false, null);
+
+channel1.BasicPublish("", "order_queue", null, Encoding.UTF8.GetBytes("order1"));
+
+Console.ReadKey();
